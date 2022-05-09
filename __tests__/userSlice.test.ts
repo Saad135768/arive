7b7b3eb3a8a6b30f5f1ebc3c6d4befd5680006fc
Saad Essam <60039644+Saad135768@users.noreply.github.com>
2022-05-09 12:23:53 +0200

    setup packages needed for testing

---
+++ __tests__/userSlice.test.ts
@@ -0,0 +1,14 @@
+import { IUserState } from '../interface'
+import { userSlice } from './../redux/features/user/userSlice'
+
+
+describe('user reducer', () => {
+  const initialState: IUserState = {
+    value: [],
+    loading: false,
+  }
+  it('should handle initial state', () => {
+   
+  })
+
+})
